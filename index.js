91e20c903249d37475b25d41045f2f1ec0169764
Letícia Gomes Ribeiro <leticia_gomes@outlook.com>
2026-03-18 20:59:54 -0300

    integração ci e cd

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-function soma(a, b) {
-    return a + b;
-}
+const express = require('express');
+const soma = require('./soma');
 
-module.exports = soma;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Aplicação rodando ...');
+});
+
+app.get('/soma', (req, res) => {
+    const {a, b} = req.query;
+    const resultado = soma(Number(a), Number(b));
+
+    res.send(`Resultado: ${resultado}`);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const soma = require('./index');
+const soma = require('./soma');
 
 test('soma 2 + 2 = 4', () => {
     expect(soma(2, 2)).toBe(4);
